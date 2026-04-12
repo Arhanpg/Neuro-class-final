615f79a9f4a64c9c20c33c23fdfd4bf170c8d15c
Arhanpg <arhanghosarwade05@gmail.com>
2026-04-12 15:41:02 +0530

    basic changes

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'neuroclass-dev-secret-2024')
     MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
     MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'Poorva@07')
     MYSQL_DB = os.environ.get('MYSQL_DB', 'neuroclass')
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'uploads')
     MAX_CONTENT_LENGTH = 50 * 1024 * 1024  # 50MB
